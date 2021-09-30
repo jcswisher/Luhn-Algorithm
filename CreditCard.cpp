a6ad486815886c263b3fe90faaebf02005a0a408
jswish42 <58486055+jswish42@users.noreply.github.com>
2021-09-29 21:21:09 -0400

    Added CreditCard class

---
+++ CreditCard.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jeremiah Swisher on 9/24/21.
+//
+
+#include "CreditCard.h"
---
+++ CreditCard.h
@@ -0,0 +1,17 @@
+//
+// Created by Jeremiah Swisher on 9/24/21.
+//
+
+#ifndef PROJECT01_CREDITCARD_H
+#define PROJECT01_CREDITCARD_H
+
+
+class CreditCard {
+
+    private: int cardNumber[];
+
+    public: bool isValid() {};
+};
+
+
+#endif //PROJECT01_CREDITCARD_H
