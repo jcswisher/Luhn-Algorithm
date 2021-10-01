0149d67ea7343c76ab84913c2db4692370df1e2d
jswish42 <58486055+jswish42@users.noreply.github.com>
2021-10-01 16:36:04 -0400

    Added compiler files

---
+++ cmake-build-debug/CMakeFiles/Project01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project01.dir/CreditCard.cpp.o"
+  "CMakeFiles/Project01.dir/main.cpp.o"
+  "Project01"
+  "Project01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
