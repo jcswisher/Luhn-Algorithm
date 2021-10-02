953e4e74fe3c20faed000459e7aedd670d5f0603
jswish42 <58486055+jswish42@users.noreply.github.com>
2021-10-01 21:50:16 -0400

    Added to main function

--- main.cpp
+++ main.cpp
@@ -3,5 +3,29 @@
 using namespace std;
 int main() {
 
+    // ask for credit card numbers until the user says exit
+    while (true) {
 
+        string userInput = "";
+        string cardNumber = "";
+
+        cout << "Enter a credit card number to check: ";
+        getline(cin,userInput);
+
+        // exit the program when the user wants to
+        if (userInput == "exit") {
+            return 0;
+        }
+
+        // check each character supplied and save the digits to a new string
+        for (int i = 0; i < userInput.length(); i++) {
+            if (isdigit(userInput[i])) {
+                cardNumber += userInput[i];
+            }
+        }
+
+
+
+        cout << cardNumber << endl;
+    }
 }
