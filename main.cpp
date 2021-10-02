d03195d0587f121e456e341dd0a0cced1f021d1f
jswish42 <58486055+jswish42@users.noreply.github.com>
2021-10-01 22:23:05 -0400

    Added and updated main function

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 
     // ask for credit card numbers until the user says exit
-    while (true) {
+    infiniteLoop:while (true) {
 
         string userInput = "";
         string cardNumber = "";
@@ -19,13 +19,24 @@ int main() {
 
         // check each character supplied and save the digits to a new string
         for (int i = 0; i < userInput.length(); i++) {
-            if (isdigit(userInput[i])) {
+            if (!isdigit(userInput[i]) && userInput[i] != '-' && userInput[i] != ' ') {
+
+                // the user entered something that's not a digit or a separator like '-' and ' '
+                // tell them of their error and go back to the start of the infinite while loop
+                cout << "You entered something that is not allowed. Try again." << endl;
+                goto infiniteLoop;
+
+            } else if (isdigit(userInput[i])) {
                 cardNumber += userInput[i];
             }
         }
 
-
-
-        cout << cardNumber << endl;
+        if (cardNumber.length() < 16) {
+            cout << "You entered too few digits, try that again." << endl;
+        } else if (cardNumber.length() > 16) {
+            cout << "You entered too many digits, try that again." << endl;
+        } else {
+            // create credit card object with user supplied number
+        }
     }
 }
